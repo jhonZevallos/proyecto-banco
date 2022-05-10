fb3636d5f366d993d6d793e14dfb74271205cbb6
jhonZevallos <zevallosjhon50@gmail.com>
2022-05-10 18:35:19 -0500

    circuit-breaker

--- customer-service/src/main/java/com/customer/service/controller/CustomerController.java
+++ customer-service/src/main/java/com/customer/service/controller/CustomerController.java
@@ -1,6 +1,7 @@
 package com.customer.service.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +12,7 @@ import com.customer.service.client.Report;
 import com.customer.service.model.Customer;
 import com.customer.service.service.CustomerService;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -57,9 +59,9 @@ public class CustomerController {
 		}
 		return ResponseEntity.ok(customer);
 	}
-	
+
 	@GetMapping("/{typeCustomer}")
-	public ResponseEntity<Mono<Customer>> getCustomerByTypeCustomer(@PathVariable("typeCustomer") String typeCustomer){
+	public ResponseEntity<Mono<Customer>> getCustomerByTypeCustomer(@PathVariable("typeCustomer") String typeCustomer) {
 		Mono<Customer> customer = customerService.getCustomerByTypeCustomer(typeCustomer);
 		if (customer == null) {
 			return ResponseEntity.notFound().build();
@@ -78,15 +80,18 @@ public class CustomerController {
 		customerService.deleteCustomer(id);
 	}
 
-	// *******metodos feign******************
+	// *******metodos webclient******************
+	@CircuitBreaker(name = "accountCB", fallbackMethod = "fallBackPostAccount")
 	@PostMapping("/account/{nroDocument}")
 	public Mono<ResponseEntity<Mono<Account>>> saveAccount(@PathVariable("nroDocument") int nroDocument,
 			@RequestBody Account a) {
-		return Mono.just(ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON)
-				.body(customerService.addAccount(nroDocument, a)))
+		return Mono
+				.just(ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON)
+						.body(customerService.addAccount(nroDocument, a)))
 				.defaultIfEmpty(ResponseEntity.notFound().build());
 	}
 
+	@CircuitBreaker(name = "accountCB", fallbackMethod = "fallBackGetAccount")
 	@GetMapping("/account/{nroDocument}")
 	public ResponseEntity<Mono<Account>> getAccountByNroDocument(@PathVariable("nroDocument") int nroDocument) {
 		Mono<Account> account = customerService.getAccountByNroDoc(nroDocument);
@@ -95,46 +100,50 @@ public class CustomerController {
 		}
 		return ResponseEntity.ok(account);
 	}
-	
-	
+
+	@CircuitBreaker(name = "creditCB", fallbackMethod = "fallBackPostCredit")
 	@PostMapping("/credit/{nroDocument}")
-	public ResponseEntity<Mono<Credit>> saveCredit(@PathVariable("nroDocument") int nroDocument,@RequestBody Credit credit){
+	public ResponseEntity<Mono<Credit>> saveCredit(@PathVariable("nroDocument") int nroDocument,
+			@RequestBody Credit credit) {
 		Mono<Credit> nuevoCredit = customerService.addCredit(nroDocument, credit);
 		return ResponseEntity.ok(nuevoCredit);
 	}
-	
+
+	@CircuitBreaker(name = "creditCB", fallbackMethod = "fallBackGetCredit")
 	@GetMapping("/credit/{nroDocument}")
-	public ResponseEntity<Mono<Credit>> getCreditByNroDocument(@PathVariable("nroDocument") int nroDocument){
+	public ResponseEntity<Mono<Credit>> getCreditByNroDocument(@PathVariable("nroDocument") int nroDocument) {
 		Mono<Credit> credit = customerService.getCreditByNroDoc(nroDocument);
 		if (credit == null) {
 			return ResponseEntity.notFound().build();
 		}
 		return ResponseEntity.ok(credit);
 	}
-	
+
+	@CircuitBreaker(name = "todosCB", fallbackMethod = "fallBackGetTodos")
 	@GetMapping("/todos/{nroDocument}")
-	public ResponseEntity<Mono<Report>> getAllProducts(@PathVariable("nroDocument") int nroDocument){
+	public ResponseEntity<Mono<Report>> getAllProducts(@PathVariable("nroDocument") int nroDocument) {
 		Mono<Report> resultado = customerService.getCustomerAndProducts(nroDocument);
 		return ResponseEntity.ok(resultado);
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 
+	//***************************************************CIRCUIT BREAKER**************************************************************
+	private ResponseEntity<Flux<Account>> fallBackGetAccount(@PathVariable("nroDocument") int nroDocument,RuntimeException exception){
+		return new ResponseEntity("Falla al obtener el listado de cuentas del usuario con nroDocument : " + nroDocument,HttpStatus.OK);
+	}
+	
+	private ResponseEntity<Flux<Account>> fallBackPostAccount(@PathVariable("nroDocument") int nroDocument,@RequestBody Account a,RuntimeException exception){
+		return new ResponseEntity("Falla al insertar la cuenta del usuario con nroDocument : " + nroDocument,HttpStatus.OK);
+	}
+	
+	private ResponseEntity<Flux<Account>> fallBackGetCredit(@PathVariable("nroDocument") int nroDocument,RuntimeException exception){
+		return new ResponseEntity("Falla al obtener el listado de creditos del usuario con nroDocument : " + nroDocument,HttpStatus.OK);
+	}
+	
+	private ResponseEntity<Flux<Account>> fallBackPostCredit(@PathVariable("nroDocument") int nroDocument,@RequestBody Credit credit,RuntimeException exception){
+		return new ResponseEntity("Falla al insertar el credito del usuario con nroDocument : " + nroDocument,HttpStatus.OK);
+	}
+	
+	private ResponseEntity<Flux<Account>> fallBackGetTodos(@PathVariable("nroDocument") int nroDocument,RuntimeException exception){
+		return new ResponseEntity("Falla al obtener el listado de productos del usuario con nroDocument : " + nroDocument,HttpStatus.OK);
+	}
 }
