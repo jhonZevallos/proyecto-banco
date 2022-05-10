b60556e3dd23a90acddc12e1a38b003324752b02
jhonZevallos <zevallosjhon50@gmail.com>
2022-05-10 10:50:54 -0500

    eureka

--- credit-service/src/main/java/com/credit/service/CreditServiceApplication.java
+++ credit-service/src/main/java/com/credit/service/CreditServiceApplication.java
@@ -2,8 +2,10 @@ package com.credit.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CreditServiceApplication {
 
 	public static void main(String[] args) {
--- customer-service/src/main/java/com/customer/service/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/customer/service/CustomerServiceApplication.java
@@ -2,9 +2,10 @@ package com.customer.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
